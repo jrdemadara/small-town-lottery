d9816e1edea78ee47cfd9255346dd30462c34ba8
Johnny Roger <johnnyrogers.demadara@protonmail.com>
2023-01-08 00:33:14 +0800

    added undermaintenance feature

---
+++ app/src/main/java/com/slicksoftcoder/smalltownlottery/features/undermaintenance/UnderMaintenanceActivity.kt
@@ -0,0 +1,11 @@
+package com.slicksoftcoder.smalltownlottery.features.undermaintenance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnderMaintenanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_under_maintenance)
+    }
+}
